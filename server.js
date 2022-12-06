dd952c9ebb0fd2773cb055bb7f01203ffcbecfaa
Milos Mancic <miolos@gmail.com>
2022-12-06 19:43:08 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.send('ok');
+})
+
+app.listen(3000, () => {
+    console.log('Listening to port 3000');
+})
